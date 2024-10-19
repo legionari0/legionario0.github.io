8bde9d75d5184fb9562147ce0299062ac887144c
legionari0 <avalencia312000@gmail.com>
2024-10-19 12:33:58 -0500

    Delete Scripts/menu.js

--- Scripts/menu.js
+++
@@ -1,9 +0,0 @@
-// scripts/menu.js
-document.addEventListener("DOMContentLoaded", function () {
-    const menuToggle = document.getElementById("menu-toggle");
-    const nav = document.querySelector("nav");
-
-    menuToggle.addEventListener("click", function () {
-        nav.style.display = nav.style.display === "block" ? "none" : "block";
-    });
-});
